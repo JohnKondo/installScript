88f0736ec31d1e84d1bd1e9ebc15db3be63ee63c
John <>
2024-08-30 11:39:56 +0200

    install init

---
+++ install.bat
@@ -0,0 +1,26 @@
+@echo off
+winget install -e --id JetBrains.Toolbox
+
+winget install -e --id Microsoft.VisualStudioCode
+
+winget install -e --id Docker.DockerDesktop
+
+winget install -e --id Git.Git
+
+winget install -e --id Google.Chrome
+
+winget install -e --id Google.GoogleDrive
+
+winget install -e --id SlackTechnologies.Slack
+
+winget install -e --id KeePassXCTeam.KeePassXC
+
+winget install -e --id dbeaver.dbeaver
+
+winget install -e --id Atlassian.Sourcetree
+
+winget install -e --id SmartBear.SoapUI
+
+winget install -e --id 7zip.7zip
+
+winget install -e --id Postman.Postman
